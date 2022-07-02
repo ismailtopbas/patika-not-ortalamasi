767a15063858309d21cf2aecb5e311b3156143a0
İsmail <ismailtopbas1993@gmail.com>
2022-07-02 19:40:25 +0300

    not ortalaması tamamlandı

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]   args){
+
+        int mat,fizik,kimya,turkce,tarih,muzik;
+
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Fizik Notunuz :");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz :");
+        kimya = inp.nextInt();
+
+        System.out.print("Türkçe Notunuz :");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz :");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Notunuz :");
+        muzik = inp.nextInt();
+
+        System.out.print("Matematik Notunuz :");
+        mat = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + muzik + tarih);
+        double sonuc = toplam / 6.0;
+        boolean kosul1 = sonuc >= 60;
+        boolean kosul2 = sonuc <= 60;
+        boolean kosulsonuc = kosul1 || kosul2;
+        String ortalama = kosulsonuc ? " Geçti" : " Kaldı";
+
+        System.out.println("Ortalamanız:" +  sonuc + ortalama);
+
+
+
+
+
+    }
+}
